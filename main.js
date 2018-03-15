50e2a535f18885a26689e9761f26b949d696ae2b
Abhishek Ranjan <abhi03iiitm@gmail.com>
2018-03-16 00:13:18 +0530

    electron

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain, shell} = require('electron');
 const path = require('path');
 const url = require('url');
+const _ = require('lodash');
+const ffmpeg = require('fluent-ffmpeg');
 
-let mainWin;
+
+
+let mainWindow;
 
 app.on('ready', () => {
-  mainWin = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {backgroundThrottling: false}
   });
 
-  mainWin.loadURL(url.format({
+  mainWindow.loadURL(url.format({
      pathname: path.join(__dirname, '/src/index.html'),
      protocol: 'file',
      slashes: true
   }));
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises)
+    .then((results) => {
+      mainWindow.webContents.send('metadata:complete', results);
+    });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name)[0];
+    const outputName = video.name.split('.')[0]
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('progress', ({ timemark }) =>
+        mainWindow.webContents.send('conversion:progress', { video, timemark })
+      )
+      .on('end', () =>
+        mainWindow.webContents.send('conversion:end', { video, outputPath })
+      )
+      .run();
+  });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
 });
